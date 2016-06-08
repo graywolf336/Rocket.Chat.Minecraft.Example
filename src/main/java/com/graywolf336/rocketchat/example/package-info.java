034c0ab6d1f17506bb3d732659289892dbb904b4
graywolf336 <graywolf336@craftyn.com>
2016-06-07 21:04:15 -0500

    Actually change the package-info.java files, whoops

--- src/main/java/com/graywolf336/rocketchat/example/package-info.java
+++ src/main/java/com/graywolf336/rocketchat/example/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
- */
-/**
- * @author Administrator
+ * Represents the main package of the Rocket.Chat.Minecraft.Example plugin.
  *
+ * @author graywolf336
+ * @since 1.0.0
+ * @version 1.0.0
  */
 package com.graywolf336.rocketchat.example;
